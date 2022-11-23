50c8ccdf081238532bd844f4fb4eb06448623017
CROW_07 <100528047+HuyusakiYasiti@users.noreply.github.com>
2022-11-24 01:38:37 +0900

    Add files via upload

---
+++ insertionSorter2.py
@@ -0,0 +1,52 @@
+import random, time
+
+
+lis = [random.randint(0, 9) for x in range(10)]
+
+print(lis)
+
+
+
+
+l = len(lis)
+
+ins = 1
+
+
+
+
+while ins < l:
+
+    temp = lis[ins]
+
+    cmp = ins - 1
+
+
+    while cmp >= 0:
+
+        if temp < lis[cmp]:
+
+            lis[cmp + 1] = lis[cmp]
+
+            print(lis)
+
+        else:
+
+            break
+
+
+        cmp = cmp - 1
+
+
+
+
+    lis[cmp + 1] = temp
+
+    print(lis)
+
+
+    ins = ins + 1
+
+
+
+
